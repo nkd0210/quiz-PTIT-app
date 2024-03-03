7fb97219509f3045b663d8666fb300e4ff046b18
Nguyễn Kim Dũng <kimdungvn52@gmail.com>
2024-03-03 17:17:33 +0700

    them tinh nang cho trang main va trang profile

---
+++ assets/js/profile.js
@@ -0,0 +1,15 @@
+const tabs = document.querySelectorAll(".tab-item");
+const lists = document.querySelectorAll(".main-list");
+
+tabs.forEach(function (tab, index) {
+  const list = lists[index];
+  tab.onclick = (e) => {
+    e.preventDefault();
+    document.querySelector(".tab-item.active").classList.remove("active");
+    document.querySelector(".main-list.active").classList.remove("active");
+
+    tab.classList.add("active");
+    list.classList.add("active");
+    
+  };
+});
